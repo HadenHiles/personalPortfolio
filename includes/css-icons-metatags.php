f4847aed788d5f3449ae863e981547f1fcb0a778
Haden <HadenHiles@gmail.com>
2014-09-17 22:15:30 -0400

    Added an icon for both desktop and mobile devices

--- includes/css-icons-metatags.php
+++ includes/css-icons-metatags.php
@@ -1,5 +1,10 @@
-//Include external stylesheets
+// Include external stylesheets
 <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
 
+// Meta tags to be rendered
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+// Icons
+<link rel="icon" type="images/x-icon" href="../images/icons/favicon.ico" />
+<link rel="apple-touch-icon" href="../images/icons/apple-touch-icon.png">
