f4fa8928215371f969dfaf6229c95d834e884bed
Haden <HadenHiles@gmail.com>
2014-09-17 22:06:34 -0400

    Removed all polymer aspects of the website
    Starting to build site with only bootstrap. Polymer prevents the use of
    frameworks within it.

---
+++ includes/css-icons-metatags.php
@@ -0,0 +1,5 @@
+//Include external stylesheets
+<link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Haden Hiles | Portfolio</title>
+
+    <!--Include external stylesheets-->
+    <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
+
+    <!--Include external javascript files-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?
+        include($_SERVER['DOCUMENT_ROOT'].'/templates/header.php');
+    ?>
+</body>
+</html>
