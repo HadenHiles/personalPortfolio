3480bae1634f0b2a9cdc14c3c4bb9065cf9c7abd
Haden <HadenHiles@gmail.com>
2014-09-17 22:42:56 -0400

    Added php file which will contain all necessary javascript files that need to be included in the site - Got icons working except for in chrome.

--- includes/css-icons-metatags.php
+++ includes/css-icons-metatags.php
@@ -1,10 +1,11 @@
-// Include external stylesheets
+<!--Include external stylesheets-->
 <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
 
-// Meta tags to be rendered
+<!--Meta tags to be rendered-->
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-// Icons
-<link rel="icon" type="images/x-icon" href="../images/icons/favicon.ico" />
-<link rel="apple-touch-icon" href="../images/icons/apple-touch-icon.png">
+<!--Icons-->
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="icon" type="image/png" href="apple-touch-icon.png">
+<link rel="apple-touch-icon" href="apple-touch-icon.png" />
--- index.php
+++ index.php
@@ -2,17 +2,14 @@
 <html>
 <head>
     <title>Haden Hiles | Portfolio</title>
-
-    <!--Include external stylesheets-->
-    <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
-
-    <!--Include external javascript files-->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    <?
+        require_once($_SERVER['DOCUMENT_ROOT'].'/includes/css-icons-metatags.php');
+        require_once($_SERVER['DOCUMENT_ROOT'].'/includes/support_files.php');
+    ?>
 </head>
 <body>
     <?
-        include($_SERVER['DOCUMENT_ROOT'].'/templates/header.php');
+        require_once($_SERVER['DOCUMENT_ROOT'].'/templates/header.php');
     ?>
 </body>
 </html>
