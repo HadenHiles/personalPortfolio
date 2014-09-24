3bc91c14571588a5631b007b3f18c65d65e9753e
Haden <HadenHiles@gmail.com>
2014-09-23 22:51:00 -0400

    -style additions and animations for the connect with me sidebar -added links to each social platform

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 <body>
     <div class="social">
         <ul>
-            <li><a href=""><i class="fa fa-github-square"></i></a></li>
-            <li><a href=""><i class="fa fa-linkedin-square"></i></a></li>
-            <li><a href=""><i class="fa fa-twitter-square"></i></a></li>
-            <li><a href=""><i class="fa fa-facebook-square"></i></a></li>
+            <li><a href="https://github.com/HadenHiles"><i class="fa fa-github-square"></i></a></li>
+            <li><a href="https://www.linkedin.com/pub/haden-hiles/82/108/676"><i class="fa fa-linkedin-square"></i></a></li>
+            <li><a href="https://twitter.com/HadenHiles"><i class="fa fa-twitter-square"></i></a></li>
+            <li><a href="https://www.facebook.com/haden.hiles"><i class="fa fa-facebook-square"></i></a></li>
             <li><a href=""><i class="fa fa-google-plus-square"></i></a></li>
             <li><a href=""><i class="fa fa-pinterest-square"></i></a></li>
         </ul>
