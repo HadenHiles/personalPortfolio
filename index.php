ee665a2f36d7976aa38121fee8fa566e746043b0
Haden <HadenHiles@gmail.com>
2014-09-23 23:38:47 -0400

    -minor style tweaks -connected bar now has cool animations

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
             <li><a href="https://www.linkedin.com/pub/haden-hiles/82/108/676"><i class="fa fa-linkedin-square"></i></a></li>
             <li><a href="https://twitter.com/HadenHiles"><i class="fa fa-twitter-square"></i></a></li>
             <li><a href="https://www.facebook.com/haden.hiles"><i class="fa fa-facebook-square"></i></a></li>
-            <li><a href=""><i class="fa fa-google-plus-square"></i></a></li>
-            <li><a href=""><i class="fa fa-pinterest-square"></i></a></li>
+            <li><a href="https://plus.google.com/106821251778382631071/"><i class="fa fa-google-plus-square"></i></a></li>
+            <li><a href="http://www.pinterest.com/hadenhiles/"><i class="fa fa-pinterest-square"></i></a></li>
         </ul>
     </div>
     <?
