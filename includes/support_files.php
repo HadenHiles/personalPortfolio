ba78cbde13cf0369ffeaf55d571c04400288a207
Haden <HadenHiles@gmail.com>
2014-09-23 23:39:52 -0400

    -removed jquery sticky.js as it was not needed anymore and was just taking up unnecessary space

--- includes/support_files.php
+++ includes/support_files.php
@@ -3,5 +3,4 @@
 <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
 <script src="js/cycle2.js"></script>
 <script src="js/skrollr.js"></script>
-<script src="js/sticky.js"></script>
 <script src="js/main.js"></script>
