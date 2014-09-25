86b55e035b026f3fe14879580a5cd1d1a9d6e3a5
Haden <HadenHiles@gmail.com>
2014-09-25 15:10:02 -0400

    -made logo scroll to the top of the page just like the "home" button does

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="../images/logo/stamp-light-bevel.png" class="img-responsive large_logo" /><img src="../images/logo/stamp-small-bevel-light.png" class="img-responsive small_logo" /></a>
+                <a class="navbar-brand" onclick="$('html, body').animate({scrollTop: $('body').offset().top}, 1000);"><img src="../images/logo/stamp-light-bevel.png" class="img-responsive large_logo" /><img src="../images/logo/stamp-small-bevel-light.png" class="img-responsive small_logo" /></a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
@@ -21,7 +21,7 @@
                     <li><a onclick="$('html, body').animate({scrollTop: $('#contact').offset().top - 60}, 1000);">Contact</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right icon-nav">
-                    <li class="active"><a onclick="$('html, body').animate({scrollTop: $('body').offset().top}, 1000);"><span class="glyphicon glyphicon-home"></span></a></li>
+                    <li><a onclick="$('html, body').animate({scrollTop: $('body').offset().top}, 1000);"><span class="glyphicon glyphicon-home"></span></a></li>
                     <li><a onclick="$('html, body').animate({scrollTop: $('#portfolio').offset().top - 90}, 1000);"><span class="glyphicon glyphicon-folder-open"></span></a></li>
                     <li><a onclick="$('html, body').animate({scrollTop: $('#about').offset().top - 60}, 1000);"><span class="glyphicon glyphicon-question-sign"></span></a></li>
                     <li><a onclick="$('html, body').animate({scrollTop: $('#services').offset().top - 60}, 1000);"><span class="glyphicon glyphicon-ok-sign"></span></a></li>
@@ -32,8 +32,8 @@
         </div>
     </div>
 <div class="bio">
-    <h3>Hi! My name is Haden Hiles,</h3>
-    <h3>Welcome to my website!</h3>
+    <h3>Hi! My name is Haden Hiles,<br />
+    Welcome to my website!</h3>
     <div class="bio-image">
         <img src="../images/me/haden.jpg" class="img-responsive" />
         <img src="../images/me/crazy-me-small.jpg" class="img-responsive" />
@@ -43,9 +43,13 @@
         <img src="../images/me/coldwater2.JPG" class="img-responsive" />
         <img src="../images/me/haden-guitar2.JPG" class="img-responsive" />
     </div>
-    <p>I'm trying to fill up the internet with websites.. but lets hope that I don't.</p><br />
-    <p>My goal is to keep learning. Whether it be improving my skills in sports like hockey and golf,
-    or by pumping out websites week after week. I love building and designing websites because it gives me a rewarding
-    feeling as I do it. Web design and development is my tool for creativity and I believe that it is continually moving forward,
-    which for me is reason enough to invest my time in.</p>
+</div>
+<div class="intro-message">
+    <div class="col-sm-12 col-md-10 col-lg-6">
+        <p>I'm trying to fill up the internet with websites.. but lets hope that I don't.</p><br />
+        <p>My goal is to keep learning. Whether it be improving my skills in sports like hockey and golf,
+        or by pumping out websites week after week. I love building and designing websites because it gives me a rewarding
+        feeling as I do it. Web design and development is my tool for creativity and I believe that it is continually moving forward,
+        which for me is reason enough to invest my time in.</p>
+    </div>
 </div>
