0fdd7717222e35b54c09a0b8060c9833c58fade9
Haden <HadenHiles@gmail.com>
2014-09-25 22:51:32 -0400

    -added a contact section with form posting to non existent php file

---
+++ templates/contact.php
@@ -0,0 +1,39 @@
+<div class="contact" id="contact">
+    <section id="slide-1" class="homeSlide">
+        <div class="bcg"
+            data-center="background-position: 50% 0px;"
+            data-top-bottom="background-position: 50% -100px;"
+            data-anchor-target="#slide-1"
+        >
+            <div class="hsContainer">
+                <div class="hsContent">
+                    <h2>Contact Me</h2>
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <form method="post" action="send_email.php">
+                            <fieldset>
+                                <legend>Have a Question? Reach me using the form below.</legend>
+                                <div class="col-sm-12">
+                                    <label>Name: </label><br />
+                                    <input type="text" name="name" placeholder="First & Last" required/>
+                                </div>
+                                <div class="col-sm-12">
+                                    <label>Subject: </label><br />
+                                    <input type="text" name="subject" placeholder="Subject" required/>
+                                </div>
+                                <div class="col-sm-12">
+                                    <label>Message: </label><br />
+                                    <textarea name="message" required></textarea>
+                                </div>
+                                <div class="col-sm-12">
+                                    <input type="submit" name="submit" value="Send" />
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
