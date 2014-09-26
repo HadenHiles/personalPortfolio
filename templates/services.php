d729c853e0427304c8a0a38445fc99b5d2913202
Haden <HadenHiles@gmail.com>
2014-09-25 21:32:30 -0400

    -added a services section

---
+++ templates/services.php
@@ -0,0 +1,25 @@
+<div class="services" id="services">
+    <section id="slide-1" class="homeSlide">
+        <div class="bcg"
+            data-center="background-position: 50% 0px;"
+            data-top-bottom="background-position: 50% -100px;"
+            data-anchor-target="#slide-1"
+        >
+            <div class="hsContainer">
+                <div class="hsContent">
+                    <h2>Services</h2>
+                    <div class="col-sm-4" style="padding-bottom: 2%">
+                        <img src="../images/content/web-design.png" class="img-responsive" />
+                    </div>
+                    <div class="col-sm-8">
+                        <p></p>
+                    </div>
+                    <div class="col-sm-8">
+                        <p></p>
+                    </div>
+                    <div class="col-sm-4"><img src="" class="img-responsive" /></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
