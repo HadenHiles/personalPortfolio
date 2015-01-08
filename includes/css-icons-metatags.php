0fa19eae4892ca25a82af1c642533a8ba418aaa3
Haden <HadenHiles@gmail.com>
2015-01-08 18:49:40 -0500

    -fixed media query glitch with nav and social bars

--- includes/css-icons-metatags.php
+++ includes/css-icons-metatags.php
@@ -1,5 +1,5 @@
 <!--Include external stylesheets-->
-<link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="/bootstrap-3.2.0-dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/css/styles.css" />
 <!--Must include media queries stylesheet separate from styles.css because it needs to take priority over styles.css-->
 <link rel="stylesheet" href="/css/media-queries.css" />
@@ -14,5 +14,4 @@
 <link rel="apple-touch-icon" href="apple-touch-icon.png" />
 
 <!--Fonts-->
-<link href='http://fonts.googleapis.com/css/family%3DMontserrat%7COpen%2BSans' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
